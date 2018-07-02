7773a989333e35ea79169a723656ab54b00319d9
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 11:37:58 +0200

    add city service test skeleton

---
+++ WeatherForecastTests/CityServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeatherForecastTests
+{
+    [TestClass]
+    public class CityServiceTests
+    {
+        [TestMethod]
+        public async Task Known_city_should_be_found_with_case_insensitive_search()
+        {
+        }
+
+        [TestMethod]
+        public async Task Known_city_with_country_code_should_be_found_with_case_insensitive_search()
+        {
+        }
+
+        [TestMethod]
+        public async Task Unknown_city_should_throw()
+        {
+        }
+    }
+}
