336dc05a3d6d3ac338645a521fd14d59c06cbde5
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 12:21:13 +0200

    implement forecast view model

--- WeatherForecastApp/ViewModels/ForecastViewModel.cs
+++ WeatherForecastApp/ViewModels/ForecastViewModel.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace WeatherForecastApp.ViewModels
 {
-    class ForecastViewModel
+    public class ForecastViewModel : ViewModelBase
     {
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (_title == value)
+                    return;
+
+                _title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _date;
+
+        public string Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                if (_date == value)
+                    return;
+
+                _date = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand SearchCommand => new RelayCommand(arg => OnSearch(arg as string));
+
+        private void OnSearch(string city)
+        {
+            Title = $"Weather in {city}";
+            Date = DateTime.Now.ToString("D");           
+        }
     }
 }
