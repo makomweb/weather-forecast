2acdd277be6209ed846d2743bf8d8a271834d076
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 12:33:10 +0200

    determine city id when searching

--- WeatherForecastApp/ViewModels/ForecastViewModel.cs
+++ WeatherForecastApp/ViewModels/ForecastViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using WeatherForecastLib;
 
 namespace WeatherForecastApp.ViewModels
 {
     public class ForecastViewModel : ViewModelBase
     {
+        private CityService _cityService;
+
+        public ForecastViewModel()
+        {
+            var json = File.ReadAllText(@".\Data\city-codes.json");
+            _cityService = new CityService(json);
+        }
+
         private string _title;
 
         public string Title
@@ -44,10 +54,35 @@ namespace WeatherForecastApp.ViewModels
 
         public ICommand SearchCommand => new RelayCommand(arg => OnSearch(arg as string));
 
-        private void OnSearch(string city)
+        private async void OnSearch(string city)
         {
             Title = $"Weather in {city}";
-            Date = DateTime.Now.ToString("D");           
+            Date = DateTime.Now.ToString("D");
+
+            try
+            {
+                var id = await GetCityId(city);
+
+                Title = Title + id;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private Task<int> GetCityId(string city)
+        {
+            if (city.Contains(','))
+            {
+                var parts = city.Split(',');
+
+                return _cityService.GetCityIdAsync(parts[0].Trim(), parts[1].Trim());
+            }
+
+            return _cityService.GetCityIdAsync(city);
+        }
+
         }
     }
 }
