6aa9bec2bd3ee63d84295a6886b8660d7099d0fa
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 11:11:20 +0200

    implement fetching forecast via HTTP and JSON

--- WeatherForecastLib/WeatherForecastService.cs
+++ WeatherForecastLib/WeatherForecastService.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WeatherForecastLib
 {
     public class WeatherForecastService
     {
+        public Task<HttpResponseMessage> FetchForecastAsync(string cityId)
+        {
+            var http = new HttpClient();
+            var uri = new OpenWeatherMapApi().GetServiceUri(cityId);
+            return http.GetAsync(uri);
+        }
     }
 }
--- WeatherForecastTests/WeatherForecastServiceTests.cs
+++ WeatherForecastTests/WeatherForecastServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherForecastLib;
 
 namespace WeatherForecastTests
 {
@@ -7,8 +9,12 @@ namespace WeatherForecastTests
     public class WeatherForecastServiceTests
     {
         [TestMethod]
-        public void Fetch_forecast_should_succeed()
+        public async Task Fetch_forecast_should_succeed()
         {
+            var service = new WeatherForecastService();
+            const string cityId = "2950159";
+            var forecast = await service.FetchForecastAsync(cityId);
+            Assert.IsNotNull(forecast);
         }
     }
 }
