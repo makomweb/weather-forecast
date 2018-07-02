84a1787969d9e7cb4f1220b3689584fdfa653cd2
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 12:19:38 +0200

    implement day item view model

--- WeatherForecastApp/ViewModels/ForecastDayItemViewModel.cs
+++ WeatherForecastApp/ViewModels/ForecastDayItemViewModel.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WeatherForecastLib;
 
 namespace WeatherForecastApp.ViewModels
 {
-    class ForecastDayItemViewModel
+    public class ForecastDayItemViewModel
     {
+        public string Time { get; }
+
+        public string TemperatureMaximum { get; }
+
+        public string TemperatureMinimum { get; }
+
+        public string Description { get; }
+
+        public string IconUri { get; }
+
+        public ForecastDayItemViewModel(WeatherForecastItem item)
+        {
+            Time = item.TimeUtc.ToLocalTime().ToShortTimeString();
+            TemperatureMinimum = $"{Math.Round(item.MinimumTemperatureCelsius, 1)} °C";
+            TemperatureMaximum = $"{Math.Round(item.MaximumTemperatureCelsius, 1)} °C";
+            Description = item.Description;
+            IconUri = item.Icon;
+        }
     }
 }
