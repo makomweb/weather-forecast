a6e6555f41421745626832c8d9a20c252e75fabd
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 10:57:47 +0200

    add forecast service; reference library; add unit test skeleton

--- WeatherForecastTests/WeatherForecastServiceTests.cs
+++ WeatherForecastTests/WeatherForecastServiceTests.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace WeatherForecastTests
 {
     [TestClass]
-    public class UnitTest1
+    public class WeatherForecastServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Fetch_forecast_should_succeed()
         {
         }
     }
