684b1843f34c8abc41e941fa1727065f4c821a67
Martin Komischke <martin.komischke@outlook.com>
2018-07-02 11:04:59 +0200

    add Open Weather Map API information

---
+++ WeatherForecastLib/OpenWeatherMapApi.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherForecastLib
+{
+    public class OpenWeatherMapApi
+    {
+        private const string OpenWeatherMapApiKey = /* TODO Enter your Open Weather Map API key here! */; 
+
+        public string GetServiceUri(string cityId)
+        {
+            return $"http://api.openweathermap.org/data/2.5/forecast?id={cityId}&APPID={OpenWeatherMapApiKey}";
+        }
+    }
+}
