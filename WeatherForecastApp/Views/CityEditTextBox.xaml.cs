a805115bb5b178a6ff44427c201e358b8b3ca01a
Martin Komischke <martin.komischke@outlook.com>
2018-07-11 14:35:59 +0200

    extract city edit box and parametrize it with a dependency propert to dis-/allow spaces

---
+++ WeatherForecastApp/Views/CityEditTextBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WeatherForecastApp.Views
+{
+    /// <summary>
+    /// Interaction logic for CityEditTextBox.xaml
+    /// </summary>
+    public partial class CityEditTextBox : UserControl
+    {
+        public CityEditTextBox()
+        {
+            InitializeComponent();
+            InnerTextBox.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        public static readonly DependencyProperty IsSpaceAllowedProperty = DependencyProperty.Register("IsSpaceAllowed", typeof(bool), typeof(CityEditTextBox));
+
+        public bool IsSpaceAllowed
+        {
+            get { return (bool)GetValue(IsSpaceAllowedProperty); }
+            set { SetValue(IsSpaceAllowedProperty, value); }
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs args)
+        {
+            base.OnPreviewKeyDown(args);
+            if (!IsSpaceAllowed && (args.Key == Key.Space))
+            {
+                args.Handled = true;
+            }
+        }
+    }
+}
